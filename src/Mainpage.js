ffd3f61677c621647a62c2828f3add7b23b82871
Vince Vuong <vuong.vincent@gmail.com>
2017-10-16 15:49:45 -0700

    Comment out Righthalf- result section

--- src/Mainpage.js
+++ src/Mainpage.js
@@ -29,7 +29,7 @@ class Mainpage extends React.Component {
           firstpics={this.props.firstpics}
           secondpics={this.props.secondpics}
         />
-        <div id='Righthalf'>
+        {/* <div id='Righthalf'>
           {result}
           {console.log('suggestion props: ', this.props.suggestions)}
           {
@@ -37,7 +37,7 @@ class Mainpage extends React.Component {
             this.props.suggestions &&
             <Suggestions data={this.props.suggestions} />
           }
-        </div>
+        </div> */}
       </div>
     )
   }
