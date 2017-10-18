ef59bee53205ca7480c78f099366682bca3513a2
woojunchoi <emilyhongji@Emily-Kim.local>
2017-10-18 16:06:38 -0700

    hi

--- server.js
+++ server.js
@@ -4,14 +4,16 @@ const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpack = require('webpack');
 const webpackConfig = require('./webpack.config.js');
 const app = express();
-
 const mongoose = require('mongoose');
 const Location = require('./server-mongoose/models/locationModel');
 const locationController = require('./server-mongoose/controllers/locationController');
 const yelpApi = require('./controllers/yelpApi');
+const userController = require('./server-mongoose/controllers/userController');
+
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://teamwind:teamwind@ds119585.mlab.com:19585/trvlr');
+mongoose.connect('mongodb://pinkfairyarmadillo:pinkfairyarmadillo@ds121535.mlab.com:21535/pinkfairyarmadillo');
+
 mongoose.connection.once('open', (err, success) => {
   if (err) console.log('NOOOOOOOO');
   console.log('CONNECTED YAYYYYY');
@@ -30,9 +32,10 @@ mongoose.connection.once('open', (err, success) => {
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 
- 
+app.use(bodyParser.json())
+app.use(cookieParser())
+
 const compiler = webpack(webpackConfig);
- 
 app.use(express.static(__dirname + '/www'));
  
 app.use(webpackDevMiddleware(compiler, {
@@ -54,6 +57,8 @@ app.use(function (req, res, next){
 app.get('/query', locationController.findMatches);
 app.post('/creating', locationController.create);
 app.get('/yelpdata:location', yelpApi.sendYelpReq);
+app.post('/createuser', userController.createUser);
+app.post('/login' , userController.login);
 
 const server = app.listen(3000, function() {
   const port = server.address().port;
--- src/Login.js
+++ src/Login.js
@@ -1,15 +1,37 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
+import axios from 'axios'
+import {Route, Redirect} from 'react-router';
+import Bigtable from './Bigtable.js';
+
+var that;
 class Login extends Component {
 constructor(props){
   super(props);
   this.state={
   username:'',
-  password:''
+  password:'',
+  logged:false
   }
+  this.handleClick = this.handleClick.bind(this)
  }
+
+handleClick(e) {
+  that = this;
+  axios.post('/login', {username: this.state.username, password:this.state.password})
+  .then(function(response){
+    console.log(response);
+  })
+  .then(function(){
+    console.log('hello')
+    that.setState({logged:true})
+  })
+}
+
 render() {
     return (
       <div>
@@ -32,6 +54,7 @@ render() {
                />
              <br/>
              <RaisedButton label="Submit" primary={true} style={style} onClick={(event) => this.handleClick(event)}/>
+            {(this.state.logged === true) && <Redirect to= '/Home'/>}
          </div>
          </MuiThemeProvider>
       </div>
